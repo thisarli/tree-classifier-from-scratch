51dd2c4cde77fea69f5aa2565f6f0755da1c851f
Tilman Hisarli <til.hisarli@gmail.com>
2021-10-29 18:51:27 +0100

    Finish evaluation task 3

--- cross_validation.py
+++ cross_validation.py
@@ -87,8 +87,8 @@ def accuracy(y_test, y_pred):
     """ Compute the accuracy given the ground truth and predictions
 
     Args:
-        y_gold (np.ndarray): the correct ground truth/gold standard labels
-        y_prediction (np.ndarray): the predicted labels
+        y_test (np.ndarray): the correct ground truth/gold standard labels
+        y_pred (np.ndarray): the predicted labels
 
     Returns:
         float : the accuracy
@@ -97,12 +97,148 @@ def accuracy(y_test, y_pred):
     assert len(y_test) == len(y_pred)  
     
     return np.sum(y_test == y_pred) / len(y_test)
-    
+
+
+def confusion_matrix(y_gold, y_prediction, class_labels=None):
+    """ Compute the confusion matrix.
+
+    Args:
+        y_gold (np.ndarray): the correct ground truth/gold standard labels
+        y_prediction (np.ndarray): the predicted labels
+        class_labels (np.ndarray): a list of unique class labels.
+                               Defaults to the union of y_gold and y_prediction.
+
+    Returns:
+        np.array : shape (C, C), where C is the number of classes.
+                   Rows are ground truth per class, columns are predictions
+    """
+
+    # if no class_labels are given, we obtain the set of unique class labels from
+    # the union of the ground truth annotation and the prediction
+    if not class_labels:
+        class_labels = np.unique(np.concatenate((y_gold, y_prediction)))
+
+    confusion = np.zeros((len(class_labels), len(class_labels)), dtype=np.int)
+
+    # for each correct class (row),
+    # compute how many instances are predicted for each class (columns)
+    for (i, label) in enumerate(class_labels):
+        # get predictions where the ground truth is the current class label
+        indices = (y_gold == label)
+        gold = y_gold[indices]
+        predictions = y_prediction[indices]
+
+        # quick way to get the counts per label
+        (unique_labels, counts) = np.unique(predictions, return_counts=True)
+
+        # convert the counts to a dictionary
+        frequency_dict = dict(zip(unique_labels, counts))
+
+        # fill up the confusion matrix for the current row
+        for (j, class_label) in enumerate(class_labels):
+            confusion[i, j] = frequency_dict.get(class_label, 0)
+
+    return confusion
+
+
+def precision(y_gold, y_prediction):
+    """ Compute the precision score per class given the ground truth and predictions
+
+    Also return the macro-averaged precision across classes.
+
+    Args:
+        y_gold (np.ndarray): the correct ground truth/gold standard labels
+        y_prediction (np.ndarray): the predicted labels
+
+    Returns:
+        tuple: returns a tuple (precisions, macro_precision) where
+            - precisions is a np.ndarray of shape (C,), where each element is the
+              precision for class c
+            - macro-precision is macro-averaged precision (a float)
+    """
+
+    confusion = confusion_matrix(y_gold, y_prediction)
+    p = np.zeros((len(confusion),))
+    for c in range(confusion.shape[0]):
+        if np.sum(confusion[:, c]) > 0:
+            p[c] = confusion[c, c] / np.sum(confusion[:, c])
+
+    # # Compute the macro-averaged precision
+    # macro_p = 0.
+    # if len(p) > 0:
+    #     macro_p = np.mean(p)
+
+    return p
+
+
+def recall(y_gold, y_prediction):
+    """ Compute the recall score per class given the ground truth and predictions
+
+    Also return the macro-averaged recall across classes.
+
+    Args:
+        y_gold (np.ndarray): the correct ground truth/gold standard labels
+        y_prediction (np.ndarray): the predicted labels
+
+    Returns:
+        tuple: returns a tuple (recalls, macro_recall) where
+            - recalls is a np.ndarray of shape (C,), where each element is the
+                recall for class c
+            - macro-recall is macro-averaged recall (a float)
+    """
+
+    confusion = confusion_matrix(y_gold, y_prediction)
+    r = np.zeros((len(confusion),))
+    for c in range(confusion.shape[0]):
+        if np.sum(confusion[c, :]) > 0:
+            r[c] = confusion[c, c] / np.sum(confusion[c, :])
+
+    # # Compute the macro-averaged recall
+    # macro_r = 0.
+    # if len(r) > 0:
+    #     macro_r = np.mean(r)
+
+    return r
+
+
+def f1_score(y_gold, y_prediction):
+    """ Compute the F1-score per class given the ground truth and predictions
+
+    Also return the macro-averaged F1-score across classes.
+
+    Args:
+        y_gold (np.ndarray): the correct ground truth/gold standard labels
+        y_prediction (np.ndarray): the predicted labels
+
+    Returns:
+        tuple: returns a tuple (f1s, macro_f1) where
+            - f1s is a np.ndarray of shape (C,), where each element is the
+              f1-score for class c
+            - macro-f1 is macro-averaged f1-score (a float)
+    """
+
+    precisions = precision(y_gold, y_prediction)
+    recalls = recall(y_gold, y_prediction)
+
+    # just to make sure they are of the same length
+    assert len(precisions) == len(recalls)
+
+    f = np.zeros((len(precisions),))
+    for c, (p, r) in enumerate(zip(precisions, recalls)):
+        if p + r > 0:
+            f[c] = 2 * p * r / (p + r)
+
+    return f
 
 # Running the validation
 
-def run_simple_cross_validation(n_folds, data, rg=default_rng()): # TODO make this work
+
+def run_simple_cross_validation(n_folds, data, rg=default_rng()):
     accuracies = []
+    confusion_matrices = []
+    precisions = []
+    recalls = []
+    f1_scores = []
     for i, (train_indices, test_indices) in enumerate(train_test_k_fold(n_folds, len(data), rg)):
         print(f'------------{i}--------')
         # get the dataset from the correct splits
@@ -115,4 +251,15 @@ def run_simple_cross_validation(n_folds, data, rg=default_rng()): # TODO make th
         y_pred = predict(test[:, :-1], model[0])
         acc = accuracy(y_test, y_pred)
         accuracies += [acc]
-    return np.array(accuracies)
+        cm = confusion_matrix(y_test, y_pred)
+        confusion_matrices += [cm]
+        pr = precision(y_test, y_pred)
+        precisions += [pr]
+        rc = recall(y_test, y_pred)
+        recalls += [rc]
+        f1 = f1_score(y_test, y_pred)
+        f1_scores += [f1]
+
+    return np.mean(np.array(accuracies)), np.mean(np.array(confusion_matrices), axis=0), \
+           np.mean(np.array(precisions), axis=0), np.mean(np.array(recalls), axis=0), np.mean(np.array(f1_scores), axis=0)
+
